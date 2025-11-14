0452a192efde9b7a7e837b28fb762392add0956d
Kaung Khant <k2012khant@gmail.com>
2025-11-13 16:41:28 -0800

    Updates

---
+++ _next/static/chunks/db76b098d56eb455.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK||(globalThis.TURBOPACK=[])).push(["object"==typeof document?document.currentScript:void 0,31713,e=>{"use strict";var t=e.i(43476);function n(){let e=`(function () {
+    try {
+      var p = location.pathname;
+      if (!p.endsWith('/')) p += '/';
+      location.replace(p + 'welcome/');
+    } catch (e) {}
+  })();`;return(0,t.jsxs)("main",{className:"grid min-h-svh place-items-center bg-black text-zinc-400",children:["Loading…"," ",(0,t.jsx)("noscript",{children:(0,t.jsx)("a",{href:"welcome/",children:"Continue"})}),(0,t.jsx)("script",{dangerouslySetInnerHTML:{__html:e}})]})}e.s(["default",()=>n])}]);
