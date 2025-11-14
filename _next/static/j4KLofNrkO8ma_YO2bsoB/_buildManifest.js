dbbd7d79016abf415ff4de2f1155c3a5ccd13af8
Kaung Khant <k2012khant@gmail.com>
2025-11-13 17:04:05 -0800

    Updates

--- _next/static/j4KLofNrkO8ma_YO2bsoB/_buildManifest.js
+++ _next/static/j4KLofNrkO8ma_YO2bsoB/_buildManifest.js
@@ -3,8 +3,8 @@ self.__BUILD_MANIFEST = {
     "afterFiles": [],
     "beforeFiles": [
       {
-        "source": "/3DBusinessCard//_next/:path+",
-        "destination": "/3DBusinessCard/_next/:path+"
+        "source": "/.//_next/:path+",
+        "destination": "/_next/:path+"
       }
     ],
     "fallback": []
